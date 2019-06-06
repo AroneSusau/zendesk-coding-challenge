816fd702119f96953800e699394ccaa1f09db068
Arone Susau <arone.s@live.com.au>
2019-06-06 11:35:23 +1000

    Minor refactoring

--- src/tests/HttpTicketRequest.test.js
+++ src/tests/HttpTicketRequest.test.js
@@ -3,12 +3,12 @@ const HttpTicketRequest = require('../modules/HttpTicketRequest')
 
 describe('HttpTicketRequet', () => {
   // All tickets request
-  it('HAPPY PATH - ALL TICKETS: returns an object when provided correct credentials.', async () => {
+  it('HAPPY PATH - ALL TICKETS: returns tickets array when provided correct credentials.', async () => {
     const requester = new HttpTicketRequest()
     requester.setUrlForAllTickets()
     requester.setLoginCredentialsAndHeaders('arone.s@live.com.au', 'Arone2019@')
     const list = await requester.templateFetchRequest()
-    expect(list).not.toBeNull()
+    expect(list.tickets).not.toBeNull()
   })
 
   it('fails authentication for all tickets when provided incorrect credentials.', async () => {
@@ -29,12 +29,12 @@ describe('HttpTicketRequet', () => {
   })
 
   // Single ticket request
-  it('HAPPY PATH - SINGLE TICKET: returns an object when provided correct credentials.', async () => {
+  it('HAPPY PATH - SINGLE TICKET: returns a ticket object when provided correct credentials.', async () => {
     const requester = new HttpTicketRequest()
     requester.setUrlForAllTickets(2)
     requester.setLoginCredentialsAndHeaders('arone.s@live.com.au', 'Arone2019@')
     const list = await requester.templateFetchRequest()
-    expect(list).not.toBeNull()
+    expect(list.ticket).not.toBeNull()
   })
 
   it('fails authentication for a single when provided incorrect credentials.', async () => {

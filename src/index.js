13c7e99c4d871431112dc32e5d669639d2e3642a
Arone Susau <arone.s@live.com.au>
2019-06-11 09:51:35 +1000

    Refactored index.js for readability

--- src/index.js
+++ src/index.js
@@ -5,53 +5,57 @@
   const display = new Display()
   const message = require('./components/Display/Message')
   const TicketFetcher = require('./components/TicketFetcher/TicketFetcher')
-
-  // Named Arguments
   const { MENU, EXIT, GET_ALL_TICKETS, GET_TICKET_BY_ID, TOKEN } = require('./config')
-  // Set authentication
   const requester = new TicketFetcher(TOKEN)
 
   let PROGRAM_RUNNING = true
+  let userInput
+
   display.print(message.welcome)
 
-  // Main program loop
-  while (PROGRAM_RUNNING) {
-    let userInput = display.getInput(message.main)
+  // Object containing all possible user actions.
+  const actions = {
+    [MENU]: () => {
+      display.print(message.menu)
+    },
 
-    if (userInput === GET_ALL_TICKETS) {
-      // Get all tickets
+    [EXIT]: () => {
+      display.print(message.goodbye)
+      PROGRAM_RUNNING = false
+    },
+
+    [GET_TICKET_BY_ID]: async () => {
+      const ticketId = display.getInput(message.id)
+      display.print(message.fetch)
+      display.printSingleTicket(await requester.fetchTicketById(ticketId))
+    },
+
+    [GET_ALL_TICKETS]: async () => {
       const apiCaller = await requester.fetchAllTickets()
       let scrolling = true
       display.print(message.fetch)
 
       while (scrolling) {
         const tickets = await apiCaller()
-
         if (tickets != null) {
           display.printAllTickets(tickets)
-
           if (tickets.nextPage != null) {
             // Check if user wants to continue scrolling
             scrolling = display.getInput(message.continueScrolling) == 'n' ? 0 : 1
           }
-
           display.printNextPageMessage(scrolling, tickets.nextPage)
         } else scrolling = false
       }
-    } else if (userInput === GET_TICKET_BY_ID) {
-      // Get ticket by id
-      const ticketId = display.getInput(message.id)
-      display.print(message.fetch)
-      display.printSingleTicket(await requester.fetchTicketById(ticketId))
-    } else if (userInput === MENU) {
-      // Print menu
-      display.print(message.menu)
-    } else if (userInput === EXIT) {
-      // Exit program
-      PROGRAM_RUNNING = !PROGRAM_RUNNING
-      display.print(message.goodbye)
+    }
+  }
+
+  // Main Loop
+  while (PROGRAM_RUNNING) {
+    userInput = display.getInput(message.main)
+
+    if (userInput in actions) {
+      await actions[userInput]()
     } else {
-      // Invalid Input
       display.print(message.invalidInput)
     }
   }
